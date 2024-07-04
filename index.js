0553d02e39a5b3fa3c38be45f9398633e0571311
Vaidehi Nayakpura <149557062+Vaidehi1005@users.noreply.github.com>
2024-07-04 14:54:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+/*Create a business name generator by combining list of adjectives and shop name  and another word
+Adjectives:
+Crazy
+Amazing
+Fire
+Shop name:
+Engine
+Foods
+Garments
+Another Word:
+Bros
+Limited
+Hub
+*/
+let random = Math.random()
+// 0 0.33 0.66 1 (3 intervals ma todi didhu )
+let first, second, third;
+//Lets generate the first word
+if (random < 0.33) {
+    first = "Crazy"
+}
+else if (random < 0.66 && random >= 0.33) {
+    first = "Amazing"
+}
+else {
+    first = "Fire"
+}
+//Lets generate the second word
+random = Math.random()
+if (random < 0.33) {
+    second = "Engine"
+}
+else if (random < 0.66 && random >= 0.33) {
+    second = "Foods"
+}
+else {
+    second = "Garments"
+}
+//Lets generate the third word
+random = Math.random()
+if (random < 0.33) {
+    third = "Bros"
+}
+else if (random < 0.66 && random >= 0.33) {
+    third = "Limited"
+}
+else {
+    third = "Hub"
+}
+console.log(`${first} ${second} ${third} `)
+//here we are creating the different random numbers so we get different o/p
